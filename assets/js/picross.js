7c40dfa10b0b68bccc531e4912fe0c396ec44f38
ErnestaMajute <ernesta.majute@gmail.com>
2021-01-12 18:43:36 +0000

    Fuctions added to check are light squates empty, dark filled in picross.js

--- assets/js/picross.js
+++ assets/js/picross.js
@@ -8,3 +8,24 @@ $(document).ready( function() {
         }
     });
 });
+
+function areLightSquaresEmpty(){
+    let lightSquaresLength = document.getElementsByClassName("light active").length;
+    if (lightSquaresLength === 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+function areDarkSquaresCorrect() {
+    let activatedDarkSquaresLength = document.getElementsByClassName("dark active").length;
+    let allDarkSquaresLength = document.getElementsByClassName("dark").length;
+    if (activatedDarkSquaresLength !== allDarkSquaresLength) {
+        return false;
+    }
+    else {
+        return true;
+    }
+}
