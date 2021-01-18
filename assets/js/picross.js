1338ecaca3f851abae0c746640fb781e625593eb
ErnestaMajute <ernesta.majute@gmail.com>
2021-01-18 00:25:07 +0000

    Timer added

--- assets/js/picross.js
+++ assets/js/picross.js
@@ -42,3 +42,26 @@ $(document).ready(function() {
         userWon();
     });
 });
+
+var timerVar = setInterval(countTimer, 1000);
+var totalSeconds = 0;
+function countTimer() {
+    ++totalSeconds;
+    var hour = Math.floor(totalSeconds /3600);
+    var minute = Math.floor((totalSeconds - hour*3600)/60);
+    var seconds = totalSeconds - (hour*3600 + minute*60);
+    if(hour < 10)
+        hour = "0"+hour;
+    if(minute < 10)
+        minute = "0"+minute;
+    if(seconds < 10)
+        seconds = "0"+seconds;
+        document.getElementById("timer").innerHTML = hour + ":" + minute + ":" + seconds;
+        document.getElementById("lost-time").innerHTML = hour + ":" + minute + ":" + seconds;
+        document.getElementById("win-time").innerHTML = hour + ":" + minute + ":" + seconds;
+    if (userWon())
+        myStopFunction();
+    if($("#give-up").click(function() {
+        myStopFunction();
+    }));     
+}
